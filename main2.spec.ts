324365890867ef8289ce1a6d6617be808d2b770c
Kenge <pgkusn@gmail.com>
2023-12-24 23:48:26 +0800

    finish "Reversed sequence"

--- main2.spec.ts
+++ main2.spec.ts
@@ -1,6 +1,11 @@
 import { describe, it, expect } from 'vitest'
 import * as fn from './main2'
 
+describe('Reversed sequence', function () {
+  it('應返回一個包含整數從 n 到 1 的陣列', function () {
+    expect(fn.reverseSeq(5)).toStrictEqual([5, 4, 3, 2, 1])
+  })
+})
 describe('The falling speed of petals', () => {
   it('依據花瓣的下降速度，返回從某一樹枝飄落到地面的時間 (高度固定為400公分)', () => {
     expect(fn.sakuraFall(5)).toBe(80)
--- main2.ts
+++ main2.ts
@@ -1,3 +1,13 @@
+export function reverseSeq(n: number): number[] {
+  const ary: number[] = []
+  for (let i = n; i > 0; i--) {
+    ary.push(i)
+  }
+  return ary
+
+  // or
+  // return [...Array(n)].map((e, i) => n - i)
+}
 export function sakuraFall(v: number): number {
   return v > 0 ? 400 / v : 0
 }
