9bf1d2a1e9f367a01ce0a941b9234d5953ec8383
Kenge <pgkusn@gmail.com>
2025-01-07 23:42:39 +0800

    1 kata completed

--- src/codewars.spec.ts
+++ src/codewars.spec.ts
@@ -1,5 +1,40 @@
 import * as fn from './codewars'
 
+describe('Words to Hex', () => {
+  it('應將每組文字轉換成十六進位', () => {
+    expect(fn.wordsToHex('Hello, my name is Gary and I like cheese.')).toEqual([
+      '#48656c',
+      '#6d7900',
+      '#6e616d',
+      '#697300',
+      '#476172',
+      '#616e64',
+      '#490000',
+      '#6c696b',
+      '#636865',
+    ])
+
+    expect(fn.wordsToHex('0123456789')).toEqual(['#303132'])
+
+    expect(fn.wordsToHex('ThisIsOneLongSentenceThatConsistsOfWords')).toEqual(['#546869'])
+
+    expect(fn.wordsToHex('Blah blah blah blaaaaaaaaaaaah')).toEqual([
+      '#426c61',
+      '#626c61',
+      '#626c61',
+      '#626c61',
+    ])
+
+    expect(fn.wordsToHex('&&&&& $$$$$ ^^^^^ @@@@@ ()()()()()')).toEqual([
+      '#262626',
+      '#242424',
+      '#5e5e5e',
+      '#404040',
+      '#282928',
+    ])
+  })
+})
+
 describe('Reducing by rules to get the result', () => {
   type RuleFunction = (a: number, b: number) => number
 
--- src/codewars.ts
+++ src/codewars.ts
@@ -1,5 +1,17 @@
 import _ from 'lodash/fp'
 
+export function wordsToHex(words: string): string[] {
+  const toHexCharCode = (str: string) => str.charCodeAt(0).toString(16)
+  const padEnd = (str: string) => str.padEnd(6, '0')
+  const addHashTag = (str: string) => `#${str}`
+
+  const toHex = _.pipe(
+    _.split(' '),
+    _.map(_.pipe(_.slice(0, 3), _.map(toHexCharCode), _.join(''), padEnd, addHashTag))
+  )
+  return toHex(words)
+}
+
 type RuleFunction = (a: number, b: number) => number
 
 export function reduceByRules(numbers: number[], rules: RuleFunction[]): number {
