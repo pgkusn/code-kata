e04503c1595601586446411d9c8a367dd3fa6547
Kenge <pgkusn@gmail.com>
2025-01-19 23:59:01 +0800

    add mirror function

--- src/codewars.spec.ts
+++ src/codewars.spec.ts
@@ -1,5 +1,96 @@
 import * as fn from './codewars'
 
+describe('Mirror object - tcejbo rorriM', () => {
+  it('應該要能正確鏡像單字屬性', () => {
+    const input = {
+      abc: '-',
+      xyz: '-',
+    }
+    const expected = {
+      abc: 'cba',
+      xyz: 'zyx',
+    }
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+
+  it('應該要能正確處理迴文', () => {
+    const input = {
+      arara: '-',
+      level: '-',
+      wow: '-',
+    }
+    const expected = {
+      arara: 'arara',
+      level: 'level',
+      wow: 'wow',
+    }
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+
+  it('鏡像時應該要保持原本的大小寫', () => {
+    const input = {
+      AbC: '-',
+      XyZ: '-',
+    }
+    const expected = {
+      AbC: 'CbA',
+      XyZ: 'ZyX',
+    }
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+
+  it('應該要能處理空字串屬性', () => {
+    const input = {
+      '': '-',
+    }
+    const expected = {
+      '': '',
+    }
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+
+  it('應該要能處理特殊字元', () => {
+    const input = {
+      'a@b#c': '-',
+      '123': '-',
+    }
+    const expected = {
+      'a@b#c': 'c#b@a',
+      '123': '321',
+    }
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+
+  it('不應該修改到原始物件', () => {
+    const input = {
+      test: '-',
+    }
+    const originalInput = { ...input }
+    fn.mirror(input)
+    expect(input).toEqual(originalInput)
+  })
+
+  it('應該要能處理多個相同值的屬性', () => {
+    const input = {
+      abc: '-',
+      abc2: '-',
+      cba: '-',
+    }
+    const expected = {
+      abc: 'cba',
+      abc2: '2cba',
+      cba: 'abc',
+    }
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+
+  it('應該要能處理空物件', () => {
+    const input = {}
+    const expected = {}
+    expect(fn.mirror(input)).toEqual(expected)
+  })
+})
+
 describe('Hollow array', function () {
   it('應判斷陣列是否符合 hollow 的定義', () => {
     expect(fn.isHollow([1, 2, 0, 0, 0, 3, 4])).toBe(true) // 左右各 2 個非零數字
--- src/codewars.ts
+++ src/codewars.ts
@@ -1,4 +1,21 @@
 import _ from 'lodash/fp'
+import * as R from 'ramda'
+
+export const mirror = (obj: Record<string, any>): Record<string, string> => {
+  const newObj: Record<string, string> = {}
+
+  Object.keys(obj).forEach(key => {
+    newObj[key] = R.reverse(key)
+  })
+
+  return newObj
+
+  // better
+  // return Object.keys(obj).reduce((o: Record<string, string>, k) => {
+  //   o[k] = R.reverse(k)
+  //   return o
+  // }, {})
+}
 
 export function isHollow(x: number[]): boolean {
   const zeroIndex = x.findIndex(n => n === 0)
