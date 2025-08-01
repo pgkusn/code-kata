1e6882e2688b2ee3ee4e033e34c8e636b93cce59
Kenge <pgkusn@gmail.com>
2025-08-02 01:10:01 +0800

    finish "Rainfall"

--- src/codewars.ts
+++ src/codewars.ts
@@ -1,6 +1,35 @@
 import _ from 'lodash/fp'
 import * as R from 'ramda'
 
+const getMonthlyValues = (town: string, s: string) => {
+  const toObj = ([town, value]: string[]) => ({ town, value })
+
+  const rainfallOfTown = R.pipe(
+    R.split('\n'),
+    R.map(R.pipe(R.split(':'), toObj)),
+    R.find(R.propEq(town, 'town'))
+  )(s) as { town: string; value: string }
+
+  if (!rainfallOfTown) return
+
+  return R.match(/\d+\.\d/g, rainfallOfTown.value)
+}
+export const mean = (town: string, s: string): number => {
+  const monthlyValues = getMonthlyValues(town, s)
+
+  if (!monthlyValues) return -1
+
+  return R.pipe(R.map(Number), R.sum, R.divide(R.__, 12))(monthlyValues)
+}
+export const variance = (town: string, s: string): number => {
+  const monthlyValues = getMonthlyValues(town, s)
+
+  if (!monthlyValues) return -1
+
+  const mean = R.pipe(R.map(Number), R.sum, R.divide(R.__, 12))(monthlyValues)
+  return monthlyValues.reduce((prev, curr) => prev + Math.pow(+curr - mean, 2), 0) / 12
+}
+
 export function backwardsPrime(start: number, stop: number): number[] {
   const isPrime = (n: number) => {
     for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
