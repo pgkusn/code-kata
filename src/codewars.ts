bdb3e04dd3bc6326d281c7416eb03686eaecf262
Kenge <pgkusn@gmail.com>
2025-08-14 11:57:49 +0800

    finish howMuch function

--- src/codewars.ts
+++ src/codewars.ts
@@ -2,8 +2,18 @@ import _ from 'lodash/fp'
 import * as R from 'ramda'
 
 export function howMuch(m: number, n: number): string[][] {
-  // your code
-  return []
+  const result = []
+
+  for (let i = Math.min(m, n); i <= Math.max(m, n); i++) {
+    const c = (i - 1) % 9 === 0 && (i - 1) / 9
+    const b = (i - 2) % 7 === 0 && (i - 2) / 7
+
+    if (c && b) {
+      result.push([`M: ${i}`, `B: ${b}`, `C: ${c}`])
+    }
+  }
+
+  return result
 }
 
 export function getParticipants(handshakes: number): number {
