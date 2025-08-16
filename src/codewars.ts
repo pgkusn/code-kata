a90a42984a15cc59667314a6fd6c153e24afd559
Kenge <pgkusn@gmail.com>
2025-08-16 20:59:33 +0800

    finish hammingCode function

--- src/codewars.ts
+++ src/codewars.ts
@@ -1,13 +1,36 @@
-import _ from 'lodash/fp'
+import _ from 'lodash/fp.js'
 import * as R from 'ramda'
 
 export function hammingCode() {
-  function encode(text: string): string {
-    return 'bits'
+  const encode = (text: string): string => {
+    return R.pipe(
+      R.split(''),
+      R.map(
+        R.pipe(
+          x => x.charCodeAt(0),
+          x => x.toString(2),
+          x => x.padStart(8, '0'),
+          R.split(''),
+          R.reduce((prev, curr: string) => prev + curr.repeat(3), '')
+        )
+      ),
+      R.join('')
+    )(text)
   }
 
-  function decode(bits: string): string {
-    return 'text'
+  const decode = (bits: string): string => {
+    const decodeChunk = ([...str]) => {
+      return str.filter(x => x === '0').length >= 2 ? '0' : '1'
+    }
+
+    return R.pipe(
+      R.match(/\d{3}/g),
+      R.map(decodeChunk),
+      R.join(''),
+      R.match(/\d{8}/g),
+      R.map(R.pipe(x => parseInt(x, 2), String.fromCharCode)),
+      R.join('')
+    )(bits)
   }
 
   return { encode, decode }
