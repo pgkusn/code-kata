b011180dd988078c787128efb24ef66bce221d63
Kenge <pgkusn@gmail.com>
2025-10-01 07:48:08 +0800

    feat: one kata completed

--- src/codewars/fish.ts
+++ src/codewars/fish.ts
@@ -1,4 +1,22 @@
 export function fish(shoal: string): number {
-  // TODO: 實作邏輯
-  return 0;
+  let currentSize = 1
+  let eatenFish = 0
+
+  const sortedFish = [...shoal].sort((a, b) => Number(a) - Number(b))
+
+  for (const f of sortedFish) {
+    const fishSize = Number(f)
+
+    if (currentSize < fishSize) continue
+
+    eatenFish += fishSize
+    const target = currentSize * 4
+
+    if (eatenFish >= target) {
+      currentSize++
+      eatenFish -= target
+    }
+  }
+
+  return currentSize
 }
