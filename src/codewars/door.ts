e218bc0a9c0a66bf41399b345c5b8adcbcf524e5
Kenge <pgkusn@gmail.com>
2025-10-12 11:12:05 +0800

    feat: one kata completed

--- src/codewars/door.ts
+++ src/codewars/door.ts
@@ -1,3 +1,53 @@
 export function door(events: string): string {
-  return ''
+  let output = ''
+  let position = 0
+  let isMoving = false
+  let direction = 1
+
+  const move = () => {
+    position += direction
+  }
+
+  const changeDirection = () => {
+    direction *= -1
+  }
+
+  const stopMoving = () => {
+    isMoving = false
+    changeDirection()
+  }
+
+  const toggle = () => {
+    if (!isMoving) move()
+    isMoving = !isMoving
+  }
+
+  const stop = () => {
+    if (isMoving) {
+      changeDirection()
+      move()
+    }
+  }
+
+  const noEvent = () => {
+    if (isMoving) move()
+  }
+
+  const eventFn = {
+    P: toggle,
+    O: stop,
+    '.': noEvent,
+  }
+  type EventKey = keyof typeof eventFn
+
+  for (const e of events) {
+    eventFn[e as EventKey]()
+    output += position
+
+    if ((isMoving && position === 0) || (isMoving && position === 5)) {
+      stopMoving()
+    }
+  }
+
+  return output
 }
